4e913165c0c4b7987c2cd174889cfff6c8f6c16f
ahsanyousaf2 <ahsanyousaf2@users.noreply.github.com>
2026-06-26 08:39:36 +0500

    Ignore onnxruntime-node in webpack to fix native .node binary build error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /onnxruntime-node$/ }));
+    return config;
+  },
 };
 
 module.exports = nextConfig;
